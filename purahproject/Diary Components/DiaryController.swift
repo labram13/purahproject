d1d297243e1f2841fad2a7e35a291b2f554dc43c
MiaPham2520 <mia2520@uw.edu>
2024-06-02 18:49:50 -0700

    Added todo list functionality

--- purahproject/Diary Components/DiaryController.swift
+++ purahproject/Diary Components/DiaryController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var todoView: UINavigationController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        todoView = instantiate(id: "todo")
+        switchViewController(nil, to: todoView)
     }
 
+    func instantiate<T>(id: String) -> T! {
+        return storyboard?.instantiateViewController(withIdentifier: id) as? T
+    }
+    
+    fileprivate func switchViewController(_ from: UIViewController?, to: UIViewController) {
+        // Remove the old....
+        if from != nil {
+            from!.willMove(toParent: nil)
+            from!.view.removeFromSuperview()
+            from!.removeFromParent()
+        }
 
+        // ... swap in the new
+        self.addChild(to)
+        self.view.insertSubview(to.view, at: 0)
+        to.didMove(toParent: self)
+    }
 }
 
--- purahproject/Todo Components/TodoController.swift
+++ purahproject/Todo Components/TodoController.swift
@@ -2,28 +2,93 @@
 //  TodoController.swift
 //  purahproject
 //
-//  Created by Michaelangelo Labrador on 5/22/24.
+//  Created by Mia Pham on 5/22/24.
 //
 
 import UIKit
 
-class TodoController: UIViewController {
-
+class TodoController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    private let table: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    var items = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.items = UserDefaults.standard.stringArray(forKey: "items") ?? []
+        title = "To Do List"
+        view.addSubview(table)
+        table.dataSource = self
+        table.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        table.frame = view.bounds
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
     }
-    */
-
+    
+    @objc private func didTapAdd() {
+        let alert = UIAlertController(title: "New Task", message: "Enter a new task", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addTextField{ field in
+            field.placeholder = "Enter task"
+        }
+        alert.addAction(UIAlertAction(title: "Done", style: .default, handler: { [weak self] (_) in
+            if let field = alert.textFields?.first {
+                if let text = field.text, !text.isEmpty {
+                    print(text)
+                    DispatchQueue.main.async {
+                        var currentItems = UserDefaults.standard.stringArray(forKey: "items") ?? []
+                        currentItems.append(text)
+                        UserDefaults.standard.setValue(currentItems, forKey: "items")
+                        self?.items.append(text)
+                        self?.table.reloadData()
+                    }
+                }
+            }
+        }))
+        present(alert, animated: true)
+    }
+ 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let alert = UIAlertController(title: "Edit Task", message: "Edit the task", preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.text = self.items[indexPath.row]
+        }
+        alert.addAction(UIAlertAction(title: "Update", style: .default, handler: { [weak self] (_) in
+            if let field = alert.textFields?.first {
+                if let text = field.text, !text.isEmpty {
+                    DispatchQueue.main.async {
+                        self?.items[indexPath.row] = text
+                        tableView.reloadData()
+                        UserDefaults.standard.setValue(self?.items, forKey: "items")
+                    }
+                }
+            }
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            items.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            UserDefaults.standard.setValue(items, forKey: "items")
+        }
+    }
 }
